262ebb63deb3f2e08c1a33c4baacdd3e1007b77b
juliorgm1 <julio.developer1@gmail.com>
2020-11-04 17:00:27 -0300

    Autenticação

--- app/src/main/java/com/example/logqualy/MainActivity.java
+++ app/src/main/java/com/example/logqualy/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.logqualy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
     private FirebaseAuth mAuth;
     private TextView textViewCriarNovaConta;
+    private EditText editTextTextEmailAddress;
+    private EditText editTextTextPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
         textViewCriarNovaConta = findViewById(R.id.textViewCriarNovaConta);
         textViewCriarNovaConta.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +47,40 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextTextEmailAddress.getText().toString();
+                String password = editTextTextPassword.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this,new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Erro ao logar",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null){
+            Intent intent = new Intent(this, ProductListActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/logqualy/ProductListActivity.java
+++ app/src/main/java/com/example/logqualy/ProductListActivity.java
@@ -1,26 +1,65 @@
 package com.example.logqualy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ProductListActivity extends AppCompatActivity {
 
+    private FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
-        //FirebaseAuth.getInstance().signOut();
-
-        if (FirebaseAuth.getInstance().getCurrentUser() == null){
+        if (user == null){
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_item_logout:
+                FirebaseAuth.getInstance().signOut();
+                goToLoginActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+//        if (item.getItemId()==R.id.menu_item_logout){
+//            FirebaseAuth.getInstance().signOut();
+//            return true;
+//        }else{
+//            return super.onOptionsItemSelected(item);
+//        }
+
+    }
+
+    private void goToLoginActivity() {
+        startActivity(new Intent(
+                this,
+                MainActivity.class
+        ));
+        finish();
     }
 }
